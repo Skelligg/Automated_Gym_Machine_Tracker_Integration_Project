e6da3a17d3b005fe1df6b7d33f89af098bdabfca
Philipe Souza <philipe.souza@student.kdg.be>
2024-11-18 13:36:13 +0100

    changing url / adding *http* to it pt.6

--- JavaSpring/src/main/java/be/kdg/integration3/easyrep/repository/ArduinoRepositoryImpl.java
+++ JavaSpring/src/main/java/be/kdg/integration3/easyrep/repository/ArduinoRepositoryImpl.java
@@ -10,7 +10,7 @@ import java.util.Set;
 public class ArduinoRepositoryImpl {
 
     // this would retrieve from the database the MACHINE_ID and the ipaddress from the machine/arduino
-    private Map<String, String> arduinoInfo = new HashMap<>(Map.of("5000", null));
+    private Map<String, String> arduinoInfo = new HashMap<>(Map.of("5000", ""));
 
     public void createArduinoInfo(String id, String ipAddress) {
         arduinoInfo.put(id, ipAddress);
