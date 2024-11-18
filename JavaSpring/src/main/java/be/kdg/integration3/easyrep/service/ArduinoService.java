20355894b3779a2560a051e55151111267276ec7
Philipe Souza <philipe.souza@student.kdg.be>
2024-11-18 13:32:39 +0100

    changing url / adding *http* to it pt.5

--- JavaSpring/src/main/java/be/kdg/integration3/easyrep/service/ArduinoService.java
+++ JavaSpring/src/main/java/be/kdg/integration3/easyrep/service/ArduinoService.java
@@ -5,16 +5,13 @@ import be.kdg.integration3.easyrep.repository.ArduinoRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 @Service
 public class ArduinoService {
 
     public ArduinoRepositoryImpl arduinoRepository;
 
-    @Autowired
     public ArduinoService(ArduinoRepositoryImpl arduinoRepository) {
         this.arduinoRepository = arduinoRepository;
     }
