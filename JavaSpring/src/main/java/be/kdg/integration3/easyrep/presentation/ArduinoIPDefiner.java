837eaf9da3998455bbcc1ae3c1678dbaf3e85a21
Philipe Souza <philipe.souza@student.kdg.be>
2024-11-18 14:24:49 +0100

    changing url / adding *http* to it pt.8

--- JavaSpring/src/main/java/be/kdg/integration3/easyrep/presentation/ArduinoIPDefiner.java
+++ JavaSpring/src/main/java/be/kdg/integration3/easyrep/presentation/ArduinoIPDefiner.java
@@ -4,22 +4,31 @@ import be.kdg.integration3.easyrep.repository.ArduinoRepositoryImpl;
 import be.kdg.integration3.easyrep.service.ArduinoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/registerIP")
+//@RequestMapping("/registerIP")
+@RequestMapping("/")
 public class ArduinoIPDefiner {
     private Logger logger = LoggerFactory.getLogger(ArduinoIPDefiner.class);
     public ArduinoService arduinoService;
 //  database class
 
+    // Constructor injection of ArduinoService
+    @Autowired
+    public ArduinoIPDefiner(ArduinoService arduinoService) {
+        this.arduinoService = arduinoService;
+    }
+
     //adding checking id with database
-    @GetMapping
+    @GetMapping("/registerIP")
     public void registerIP(@RequestParam String id, @RequestParam String arduinoIP) {
         arduinoService.setArduinoInfo(id, arduinoIP);
+        logger.info("Registered IP for ID: {}", id);
     }
 
 }
--- JavaSpring/src/main/java/be/kdg/integration3/easyrep/service/ArduinoService.java
+++ JavaSpring/src/main/java/be/kdg/integration3/easyrep/service/ArduinoService.java
@@ -2,6 +2,9 @@ package be.kdg.integration3.easyrep.service;
 
 import be.kdg.integration3.easyrep.model.sessions.MachineSet;
 import be.kdg.integration3.easyrep.repository.ArduinoRepositoryImpl;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,28 +12,29 @@ import java.util.Map;
 
 @Service
 public class ArduinoService {
-
     public ArduinoRepositoryImpl arduinoRepository;
+    Logger logger = LoggerFactory.getLogger(ArduinoService.class);
 
+    @Autowired
     public ArduinoService(ArduinoRepositoryImpl arduinoRepository) {
         this.arduinoRepository = arduinoRepository;
     }
 
-
     public void setArduinoInfo(String id, String ipAddress) {
-        try{
+        try {
             arduinoRepository.setIpAddress(id, ipAddress);
-        } catch (Error e){
-            System.out.println("whatever" + e.getMessage());
+        } catch (Exception e) {
+            logger.error("could not input the received data into the system {}", e.getMessage());
         }
     }
+
 
     // Getter for IP Address by ID
     public String getIpAddress(String id) {
         return arduinoRepository.getIpAddress(id);
     }
 
-    public Map<String, String> getArduinoInfo(){
+    public Map<String, String> getArduinoInfo() {
         return arduinoRepository.getArduinoInfo();
     }
 
