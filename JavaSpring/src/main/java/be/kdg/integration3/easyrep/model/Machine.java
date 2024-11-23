f3b38cedeb30258e32c5e15373eb88cc9e6dcf92
Philipe Souza <philipe.souza@student.kdg.be>
2024-11-23 17:13:44 +0100

    adding code for machine and routines plus fixing css connections

--- JavaSpring/src/main/java/be/kdg/integration3/easyrep/model/Machine.java
+++ JavaSpring/src/main/java/be/kdg/integration3/easyrep/model/Machine.java
@@ -1,12 +1,52 @@
 package be.kdg.integration3.easyrep.model;
 
 public class Machine {
-    private long id;
+    private int id;
     private String name;
     private MaintenanceAlarm alarm;
     private String imageAddress;
 
+
+    public Machine(int id, String name, MaintenanceAlarm alarm, String imageAddress) {
+        this.id = id;
+        this.name = name;
+        this.alarm = alarm;
+        this.imageAddress = imageAddress;
+    }
+
     public void alarmSet() {
         alarm.alarmSet();
     };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MaintenanceAlarm getAlarm() {
+        return alarm;
+    }
+
+    public void setAlarm(MaintenanceAlarm alarm) {
+        this.alarm = alarm;
+    }
+
+    public String getImageAddress() {
+        return imageAddress;
+    }
+
+    public void setImageAddress(String imageAddress) {
+        this.imageAddress = imageAddress;
+    }
 }
