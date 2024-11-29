6ceb41c6d864bcdd73b7c72f2beb25c489ff3b92
Philipe Souza <philipe.souza@student.kdg.be>
2024-11-29 15:27:11 +0100

    adding param for weight in SetControllerAPI and removing commented lines

--- JavaSpring/src/main/java/be/kdg/integration3/easyrep/api/controller/SetControllerAPI.java
+++ JavaSpring/src/main/java/be/kdg/integration3/easyrep/api/controller/SetControllerAPI.java
@@ -23,24 +23,7 @@ public class SetControllerAPI {
     }
 
     @GetMapping
-    public void inputSet(@RequestParam int setNumber, @RequestParam String setTime, @RequestParam int repCount){
-//    public void inputSet(@RequestParam String startTime, String endTime, int repCount){
-//        String[] startTimeParts = startTime.split(":");
-//        int startHour = Integer.parseInt(startTimeParts[0]);
-//        int startMinute = Integer.parseInt(startTimeParts[1]);
-//        int startSecond = Integer.parseInt(startTimeParts[2]);
-//
-//        LocalTime startLocalTime = LocalTime.of(startHour, startMinute, startSecond);
-
-//        String[] endTimeParts = endTime.split(":");
-//        int endHour = Integer.parseInt(endTimeParts[0]);
-//        int endMinute = Integer.parseInt(endTimeParts[1]);
-//        int endSecond = Integer.parseInt(endTimeParts[2]);
-
-//        LocalTime endLocalTime = LocalTime.of(endHour, endMinute, endSecond);
-
-
-//        machineSetService.addSet(startLocalTime, endLocalTime, repCount);
+    public void inputSet(@RequestParam int setNumber, @RequestParam String setTime, @RequestParam int repCount, @RequestParam float weightCount){
         logger.debug("Inputting values");
         machineSetService.addSet(setNumber, setTime, repCount, weightCount);
     }
