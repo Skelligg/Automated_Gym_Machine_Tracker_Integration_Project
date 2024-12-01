054b34e892684c1dcc4e0f4159cd7cb0a2902d0b
martin <martin.ivanov@student.kdg.be>
2024-12-01 17:17:34 +0100

    added the stuff for end screen of a session and statistics; There is a slight problem with the connections with CSS and HTML

--- JavaSpring/src/main/java/be/kdg/integration3/easyrep/presentation/StatisticsController.java
+++ JavaSpring/src/main/java/be/kdg/integration3/easyrep/presentation/StatisticsController.java
@@ -1,9 +1,7 @@
 package be.kdg.integration3.easyrep.presentation;
 
-import be.kdg.integration3.easyrep.model.sessions.PlayerStatisticsDTO;
-import be.kdg.integration3.easyrep.model.sessions.RoutineSession;
-import be.kdg.integration3.easyrep.service.StatisticsService;
-import be.kdg.integration3.easyrep.service.StatisticsServiceImpl;
+import be.kdg.integration3.easyrep.model.sessions.MachineSet;
+import be.kdg.integration3.easyrep.service.MachineSetService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,34 +10,67 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @Controller
 @RequestMapping("/")
 public class StatisticsController {
     private final Logger logger = LoggerFactory.getLogger(StatisticsController.class);
 
-    private final StatisticsService statisticsService;
+
+    private final MachineSetService machineSetService;
 
     @Autowired
-    public StatisticsController(StatisticsService statisticsService) {
-        this.statisticsService = statisticsService;
+    public StatisticsController( MachineSetService machineSetService) {
+        this.machineSetService = machineSetService;
     }
+
+
 
     @GetMapping("/GymGoer/statistics")
     public String getPlayerStatistics(Model model) {
         logger.info("Mapping the statistics from the gym");
-        List<PlayerStatisticsDTO> statistics = statisticsService.getPlayerStatistics();
+        List<MachineSet> statistics = machineSetService.getAllMachineSets();
+
         logger.info("Gym goer statistics: {}", statistics);
+        List<Map<String, Object>> volumeData = new ArrayList<>();
+        for (MachineSet machineSet : statistics) {
+            Map<String, Object> volumeMap = new HashMap<>();
+            volumeMap.put("volume", machineSet.getRepCount() * machineSet.getWeightCount());
+            volumeMap.put("dateTime", machineSet.getEndTime().format(DateTimeFormatter.ofPattern("MM/dd")));
+            volumeData.add(volumeMap);
+        }
 
         model.addAttribute("statistics", statistics);
+        model.addAttribute("volumeData", volumeData);
         model.addAttribute("LocalDate", LocalDate.now());
-        model.addAttribute("volume", "10");
 
         return "GymGoer/statistics";
     }
-    @PostMapping("/GymGoer/statistics")
+
+    @GetMapping("/getChartData/{chartType}")
+    @ResponseBody
+    public List<Map<String, Object>> getChartData(@PathVariable String chartType) {
+        List<Map<String, Object>> chartData = new ArrayList<>();
+
+        for (MachineSet set : machineSetService.getAllMachineSets()) {
+            Map<String, Object> dataPoint = new HashMap<>();
+
+            dataPoint.put("dateTime", set.getEndTime().format(DateTimeFormatter.ofPattern("MM/dd")));
+            switch (chartType){
+                case "weights": dataPoint.put("value",set.getWeightCount()); break;
+                case "volume" : double volume = set.getWeightCount() * set.getRepCount();
+                dataPoint.put("value", volume); break;
+                case "reps":dataPoint.put("value", set.getRepCount()); break;
+                default: throw new IllegalArgumentException("Invalid chart " + chartType);
+            }
+            chartData.add(dataPoint);
+        }
+        return chartData;
+    }
+
+    @PostMapping("/GymGoer/statisticsClose")
     public String exitPlayerStatistics() {
         logger.info("Exiting the page");
         return "redirect:/";
--- JavaSpring/src/main/java/be/kdg/integration3/easyrep/repository/StatisticsRepository.java
+++ JavaSpring/src/main/java/be/kdg/integration3/easyrep/repository/StatisticsRepository.java
@@ -1,12 +1,13 @@
 package be.kdg.integration3.easyrep.repository;
 
+import be.kdg.integration3.easyrep.model.sessions.MachineSet;
 import be.kdg.integration3.easyrep.model.sessions.PlayerStatisticsDTO;
 
 import java.util.List;
 
 public interface StatisticsRepository {
 
-    List<PlayerStatisticsDTO> getPlayerStatistics();
+    List<MachineSet> getPlayerStatistics();
 
 
 
