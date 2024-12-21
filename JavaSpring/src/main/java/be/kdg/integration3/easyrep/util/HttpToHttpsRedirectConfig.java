d3083b1a248489be12a25ebedea00469e5b7abbe
Benjamin Lancry <benjamin.lancry@student.kdg.be>
2024-12-21 14:12:24 +0100

    added class to redirect http to https

---
+++ JavaSpring/src/main/java/be/kdg/integration3/easyrep/util/HttpToHttpsRedirectConfig.java
@@ -0,0 +1,25 @@
+package be.kdg.integration3.easyrep.util;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpToHttpsRedirectConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> redirectHttpToHttps() {
+        return factory -> factory.addAdditionalTomcatConnectors(createHttpConnector());
+    }
+
+    private Connector createHttpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+}
