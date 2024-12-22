170ed1c5c65b1b3582bf79bd65f300825f4c47b4
martin <martin.ivanov@student.kdg.be>
2024-12-22 21:25:36 +0200

    Added the read me for the python file. Moved the JS from the gymOwner index to its separate file

---
+++ JavaSpring/src/main/resources/static/js/GenderPrediction.js
@@ -0,0 +1,53 @@
+
+const data = rawData.data || [];
+
+const labels = data.map(entry=>entry.gender || 'Unknown')
+
+const dedicated = data.map(entry => entry.Dedicated || 0);
+
+const likelyToGiveUp = data.map(entry => entry["Likely to Give up"] || 0);
+
+console.log("Labels:", labels);
+console.log("Dedicated (%):", dedicated);
+console.log("Likely to Give Up (%):", likelyToGiveUp);
+
+
+const genderAnalyticsChart = document.getElementById('genderChart').getContext('2d');
+new Chart(genderAnalyticsChart, {
+    type: 'bar',
+    data: {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Dedicated (%)',
+                data: dedicated,
+                backgroundColor: 'rgba(75, 192, 192)',
+                stack: 'stack1',
+            },
+            {
+                label: 'Likely to Give Up (%)',
+                data: likelyToGiveUp,
+                backgroundColor: 'rgba(255, 99, 132)',
+                stack: 'stack1',
+            }
+        ]
+    },
+    options: {
+        responsive: true,
+        scales: {
+            x: {
+                beginAtZero: true
+            },
+            y: {
+                stacked: true,
+                beginAtZero: true,
+                max: 100
+            }
+        },
+        plugins: {
+            legend: {
+                position: 'top',
+            }
+        }
+    }
+});
